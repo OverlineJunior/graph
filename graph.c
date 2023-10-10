d21ec57d03a8b6726b0f54860419dd414839693f
over <franciscosebastianyjunior@hotmail.com>
2023-10-09 21:39:53 -0300

    Implement graph_get_adjacent_nodes

--- graph.c
+++ graph.c
@@ -3,6 +3,11 @@
 #define GRAPH_MAX_VALUE 100
 
 typedef struct {
+	int values[GRAPH_MAX_VALUE];
+	int size;
+} AdjacentNodes;
+
+typedef struct {
 	bool adjacency[GRAPH_MAX_VALUE][GRAPH_MAX_VALUE];
 	bool is_directed;
 } Graph;
@@ -35,4 +40,18 @@ void graph_del_edge(Graph *graph, int from, int to) {
 
 bool graph_has_edge(Graph graph, int from, int to) {
 	return graph.adjacency[from][to];
+}
+
+AdjacentNodes graph_get_adjacent_nodes(Graph graph, int node) {
+	AdjacentNodes adj = {
+		.size = 0,
+	};
+
+	for (int n = 0; n < GRAPH_MAX_VALUE; n++)
+		if (graph_has_edge(graph, node, n)) {
+			adj.values[adj.size] = n;
+			adj.size++;
+		}
+
+	return adj;
 }
--- test/main.c
+++ test/main.c
@@ -4,7 +4,11 @@
 int main(void) {
 	Graph g = graph_new(false);
 	graph_add_edge(&g, 2, 5);
-	printf("%i", graph_has_edge(g, 2, 5));
-	graph_del_edge(&g, 5, 2);
-	printf("%i", graph_has_edge(g, 2, 5));
+	graph_add_edge(&g, 2, 3);
+	graph_add_edge(&g, 3, 2);
+
+	AdjacentNodes adj = graph_get_adjacent_nodes(g, 2);
+
+	for (int i = 0; i < adj.size; i++)
+		printf("%i ", adj.values[i]);
 }
