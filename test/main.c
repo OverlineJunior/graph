5831f4599843b7060e4c33ff0ca7a7f0d2326598
over <franciscosebastianyjunior@hotmail.com>
2023-10-16 19:24:06 -0300

    Remake

--- test/main.c
+++ test/main.c
@@ -2,13 +2,5 @@
 #include "../graph.h"
 
 int main(void) {
-	Graph g = graph_new(false);
-	graph_add_edge(&g, 2, 5);
-	graph_add_edge(&g, 2, 3);
-	graph_add_edge(&g, 3, 2);
-
-	AdjacentNodes adj = graph_get_adjacent_nodes(g, 2);
-
-	for (int i = 0; i < adj.size; i++)
-		printf("%i ", adj.values[i]);
+	printf("Hello, world!");
 }
